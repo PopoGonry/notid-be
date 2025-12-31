53decd13503263445eebeb11fd05b9af5f2e11c2
PopoGonry <59679349+PopoGonry@users.noreply.github.com>
2025-12-31 22:28:57 +0900

    init: spring boot 프로젝트 init

---
+++ src/main/java/com/popogonry/notid/NotidApplication.java
@@ -0,0 +1,13 @@
+package com.popogonry.notid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/popogonry/notid/NotidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popogonry.notid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
