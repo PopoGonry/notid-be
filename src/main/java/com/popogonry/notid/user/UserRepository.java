e347ff8256f460088df084e7601379a080cb4a48
PopoGonry <59679349+PopoGonry@users.noreply.github.com>
2026-01-02 05:50:03 +0900

    feat: 회원가입 Service 로직 개발

--- src/main/java/com/popogonry/notid/user/UserRepository.java
+++ src/main/java/com/popogonry/notid/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com.popogonry.notid.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }
