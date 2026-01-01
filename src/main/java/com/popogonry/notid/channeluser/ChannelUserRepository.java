0254ab6ce9b739add488237b442c6bd7fd05f848
PopoGonry <59679349+PopoGonry@users.noreply.github.com>
2026-01-01 19:21:19 +0900

    feat: 저장소 계층(Repository) 개발 완료 - #6

---
+++ src/main/java/com/popogonry/notid/channeluser/ChannelUserRepository.java
@@ -0,0 +1,6 @@
+package com.popogonry.notid.channeluser;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChannelUserRepository extends JpaRepository<ChannelUser, Long> {
+}
